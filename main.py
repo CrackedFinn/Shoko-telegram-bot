e3ed78640263934921478af056691509faf6b283
Matt_Novoselov <matveynovoselov@gmail.com>
2023-01-08 12:56:46 +0300

    removed asyncio library

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import asyncio
 from aiogram import Bot, Dispatcher, executor, types
 from smsactivate.api import SMSActivateAPI
 import time
@@ -6,25 +5,28 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
-bot = Bot(token=os.getenv('TOKEN')) # Telegram bot token
+bot = Bot(token=os.getenv('TOKEN'))  # Telegram bot token
 dp = Dispatcher(bot)
-sa = SMSActivateAPI(os.getenv('SMSTOKEN')) # SMS-Activate token
-AllowedIDs = ['501667066', '1006159742'] # Make the bot private by allowing requests only from specific Telegram IDs
+sa = SMSActivateAPI(os.getenv('SMSTOKEN'))  # SMS-Activate token
+AllowedIDs = ['501667066', '1006159742']  # Make the bot private by allowing requests only from specific Telegram IDs
 
 kb = [[types.KeyboardButton(text="Новый номер для регистрации")]]
-keyboard = types.ReplyKeyboardMarkup(keyboard=kb, resize_keyboard=True, one_time_keyboard=True, input_field_placeholder="") # Create keyboard
+keyboard = types.ReplyKeyboardMarkup(keyboard=kb, resize_keyboard=True, one_time_keyboard=True,
+                                     input_field_placeholder="")  # Create keyboard
 
 
-@dp.message_handler(text="Новый номер для регистрации") # Run action after pressing keyboard
+@dp.message_handler(text="Новый номер для регистрации")  # Run action after pressing keyboard
 async def get_code(message: types.Message):
-    if str(message.chat.id) not in AllowedIDs: # Delete these 2 strings to make the bot available for everyone
+    if str(message.chat.id) not in AllowedIDs:  # Delete these 2 strings to make the bot available for everyone
         return
     if float(sa.getBalance()['balance']) < 10:  # Get balance
         msg = "😔 Недостаточно средств на балансе. Обратитесь к администратору"
         await bot.send_message(message.chat.id, msg, parse_mode="Markdown")
     else:
         number = sa.getNumberV2(service='ot', country=0)  # Get new number
-        msg = str("📞 Введите данный номер для регистрации: "+ number['phoneNumber'][0:1] +"`" + number['phoneNumber'][1:] + "`\n\n" + "💰 Цена: *"+str(int(float(number['activationCost']))) + "₽*\n\n" + "🕜 Завершите регистрацию в течении *20 мин.*")
+        msg = str("📞 Введите данный номер для регистрации: " + number['phoneNumber'][0:1] + "`" + number['phoneNumber'][
+                                                                                                  1:] + "`\n\n" + "💰 Цена: *" + str(
+            int(float(number['activationCost']))) + "₽*\n\n" + "🕜 Завершите регистрацию в течении *20 мин.*")
         await bot.send_message(message.chat.id, msg, parse_mode="Markdown")  # Get number
 
         sa.setStatus(id=number['activationId'], status=1)  # Set status ACCESS_READY
@@ -38,15 +40,18 @@ async def get_code(message: types.Message):
             if activations["activeActivations"][idfind]["activationId"] == number['activationId']:
                 msg = str("🔐 Ваш код: `" +
                           activations["activeActivations"][idfind]["smsCode"][0].replace("\n", " ").split(" ")[1] + "`")
-                await bot.send_message(message.chat.id, msg, parse_mode="Markdown", reply_markup=keyboard)  # Get SMS code
+                await bot.send_message(message.chat.id, msg, parse_mode="Markdown",
+                                       reply_markup=keyboard)  # Get SMS code
                 break
 
         sa.setStatus(id=number['activationId'], status=6)  # Finish activation, set status ACCESS_ACTIVATED
 
 
-@dp.message_handler(commands=['start']) # Run after /start command
+@dp.message_handler(commands=['start'])  # Run after /start command
 async def send_welcome(message: types.Message):
-    await bot.send_message(message.chat.id, "Чтобы получить новый номер для регистрации, нажмите на кнопку ниже", parse_mode="Markdown", reply_markup=keyboard)
+    await bot.send_message(message.chat.id, "Чтобы получить новый номер для регистрации, нажмите на кнопку ниже",
+                           parse_mode="Markdown", reply_markup=keyboard)
+
 
 if __name__ == '__main__':
     executor.start_polling(dp)
